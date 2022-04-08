9fcb81bc4bb8b4e33dddac166eed255171acb4b7
Fabrizio <fabrizioflores@ravn.co>
2022-04-08 11:24:34 -0500

    Assignment I: Memorize Completed

--- Module-1/Week-1/Memorize/Memorize/ContentView.swift
+++ Module-1/Week-1/Memorize/Memorize/ContentView.swift
@@ -7,98 +7,138 @@
 
 import SwiftUI
 
-struct ContentView: View { //(: View) to contentView, im a view
-    var emojis = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚑", "🚒", "🚐", "🛻", "🚚", "🚛", "🚜", "🛵", "🏍", "🚖", "🚝", "✈️"]
-    //How u make a var editable for the view rebuild
-    @State var emojiCount: Int = 15
-    
-    //some View, because body can have a multiple kind of views like text, hstack, vstack, etc.
-    var body: some View {
-        
-        //VStack vertically sorts all elements within it
-        VStack {
-            
-            //A scrollable view.
-            ScrollView{
-            
-                //LazyVGrid arranges its child views in a grid that grows vertically, creating items only as needed, receives a GridItem() array and this items can have modifiers
-                //Why Lazy? is lazy about accesing the body vars of all of its views
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))]){
-                    
-                    //ForEach is a kind of loop, go through an entire array, although u can give ranges
-                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                        CardView(content: emoji)
-                            //to make 2:3 aspect
-                            .aspectRatio(2/3, contentMode: .fit)
-                    }
-                }
-                .foregroundColor(.red)
-            }
-            
-            //HStack horizontally sorts all elements within it
-            HStack{
-                DeleteEmoji
-                //Spacer take all the space it can
-                Spacer()
-                Text("Shuffle").foregroundColor(.blue)
-                Spacer()
-                AddEmoji
-            }
-            .font(.largeTitle) // for icon size
-            .padding(.horizontal)
-            
-        }
-        .padding(.horizontal)
-        
+let PlacesRange: ClosedRange<Int> = 0x1F3E0...0x1F3F0
+let FoodsRange: ClosedRange<Int> = 0x1F950...0x1F95E
+let AnimalsRange: ClosedRange<Int> = 0x1F985...0x1F991
+
+func HexRangeToStringArray (range: ClosedRange<Int>) -> [String]{
+    var EmojiList: [String] = []
+    for i in range {
+        let emoji = String(UnicodeScalar(i) ?? "-")
+        EmojiList.append(emoji)
     }
-    
-    var AddEmoji : some View{
-        Button {
-            if emojiCount < emojis.count { emojiCount += 1 }
-        } label: {
-            VStack {
-                Image(systemName: "plus.app")
-            }
-        }
-    }
-    
-    var DeleteEmoji: some View{
-        Button {
-            if emojiCount > 1 { emojiCount -= 1 }
-        } label: {
-            VStack {
-                Image(systemName: "minus.square")
-            }
-        }
-    }
+    return EmojiList
 }
 
+let PlacesEmojis = HexRangeToStringArray(range: PlacesRange)
+let FoodsEmojis = HexRangeToStringArray(range: FoodsRange)
+let AnimalsEmojis = HexRangeToStringArray(range: AnimalsRange)
+
 struct CardView: View {
-    
+    var id = UUID()
     var content: String
     @State var isFaceUp: Bool = true
-    
     var body: some View {
-        
-        //ZStack overlays its children, aligning them in both axes
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20.0)
             if isFaceUp {
                 shape.fill(.white)
                 shape.strokeBorder(lineWidth: 3)
-                Text(content)
-                    .font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 shape.fill()
             }
         }
-        .onTapGesture { //instruction when touching on the element
+        .onTapGesture {
             isFaceUp = !isFaceUp
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider { //glue preview to the contentView
+func GenCardViewArray (emojis: [String]) -> [CardView]{
+    var CardViewArray: [CardView] = []
+    for i in 0..<emojis.count {
+        let TcardView = CardView(content: emojis[i], isFaceUp: true)
+        CardViewArray.append(TcardView)
+    }
+    return CardViewArray
+}
+
+func widthThatBestFits (CardCount: Int) -> CGFloat {
+    if CardCount < 5 {
+        return 130
+    } else if CardCount < 10{
+        return 90
+    } else if CardCount < 17 {
+        return 80
+    }
+    return 60
+}
+
+struct ContentView: View {
+    @State var OnScreenEmojis: [String] = PlacesEmojis
+    @State var CardViewArray: [CardView] = GenCardViewArray(emojis: PlacesEmojis)
+    @State var emojiCount: Int = Int.random(in: 4...PlacesEmojis.count)
+    var body: some View {
+        VStack {
+            Text("Memorize!")
+                .padding(.all)
+                .font(.largeTitle)
+            ScrollView{
+                LazyVGrid(
+                    columns: [
+                        GridItem(
+                            .adaptive(
+                                minimum: widthThatBestFits(CardCount: emojiCount)
+                            )
+                        )
+                    ]
+                ){
+                    ForEach(CardViewArray[0..<emojiCount], id: \.id) { CardViewEmoji in
+                        CardViewEmoji.aspectRatio(2/3, contentMode: .fit)
+                    }
+                }.foregroundColor(.red)
+            }
+            HStack{
+                SetPlaces.padding(.horizontal)
+                SetFoods.padding(.horizontal)
+                SetAnimals.padding(.horizontal)
+            }
+            .font(.title)
+            .padding(.horizontal)
+        }
+        .padding(.horizontal)
+        
+    }
+    var SetPlaces: some View{
+        Button {
+            OnScreenEmojis = PlacesEmojis.shuffled()
+            CardViewArray = GenCardViewArray(emojis: OnScreenEmojis)
+            emojiCount = Int.random(in: 4...CardViewArray.count)
+        } label: {
+            VStack {
+                Image(systemName: "house.fill")
+                Text("Places").font(.subheadline)
+            }
+        }
+    }
+    var SetFoods: some View{
+        Button {
+            OnScreenEmojis = FoodsEmojis.shuffled()
+            CardViewArray = GenCardViewArray(emojis: OnScreenEmojis)
+            emojiCount = Int.random(in: 4...CardViewArray.count)
+        } label: {
+            VStack {
+                Image(systemName: "fork.knife")
+                Text("Foods").font(.subheadline)
+            }
+        }
+    }
+    var SetAnimals: some View{
+        Button {
+            OnScreenEmojis = AnimalsEmojis.shuffled()
+            CardViewArray = GenCardViewArray(emojis: OnScreenEmojis)
+            emojiCount = Int.random(in: 4...CardViewArray.count)
+        } label: {
+            VStack {
+                Image(systemName: "pawprint.fill")
+                Text("Animals").font(.subheadline)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView()
